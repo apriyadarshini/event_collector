35212496f645ca86f8737d4f08865e87c0aabd35
apriyadarshini <50662194+apriyadarshini@users.noreply.github.com>
2019-08-06 12:39:53 +0100

    Update utils.py

--- EventCollector/events/utils.py
+++ EventCollector/events/utils.py
@@ -17,14 +17,7 @@ def collect_event_source(event,ip):
     db.session.commit()
     jobid = Jobstatus.query.filter_by(status='SUBMITTED').order_by(Jobstatus.date.desc()).first()
     if event == 'ssh attack':
-        pass #This section has not been implemented as the requirement regarding 
-    #what kind of events needs to be collected was not given
-    #currently I am assuming event would be like a name predefined by us and given to the customer.
-    #e.g. 'ssh attack' and this is what the customer would enter in the text box.
-    #If that is the way customer enters events, this block of code runs that part of code in the background 
-    #(in this case doing a grep on /var/log/auth.log for "Failed password" text) and updates our events table with the data. This also updates the Jobstatus table's status column to 
-    #'COMPLETED' once the events are collected.
-    #This is again under the assumption that we have the credentials of the system whose ip is passed by the user
+        pass 
      
     else:
         abort(400)
